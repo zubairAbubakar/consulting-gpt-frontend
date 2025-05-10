070c5cfbd04662d179785d7457cb71e335432b59
Zubair Abubakar <zuby52@gmail.com>
2025-05-10 18:42:47 +0100

    [CONGPT-6] Add Prettier and Husky configuration with pre-commit hooks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 const config: Config = {
   content: [
@@ -71,5 +72,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 };
+
+export default config;
